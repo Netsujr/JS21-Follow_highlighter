11bf62617beb142308f77914241f233c31dcccfa
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-03 17:38:28 +0900

    highlight follows the mouse

---
+++ app.js
@@ -0,0 +1,15 @@
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect();
+  console.log(linkCoords);
+  highlight.style.width = `${linkCoords.width}px`;
+  highlight.style.height = `${linkCoords.height}px`;
+  highlight.style.transform = `translate(${linkCoords.x}px, ${linkCoords.y}px)`;
+
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', highlightLink));
